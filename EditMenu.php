a362d0202a7be41f2c8727d503b3046120636c6d
beruang43221 <fabianusjeriko@gmail.com>
2022-12-08 12:02:21 +0700

    test123

---
+++ EditMenu.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Menu Admin</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  </head>
+  <body>
+    <!-- NAVBAR -->
+    <nav class="navbar bg-secondary">
+        <div class="container justify-content-end">
+            <a class="navbar-brand text-white">ADMIN</a>
+        </div>
+    </nav>
+    <!-- AKHIR NAVBAR -->
+
+    <div class="container-fluid  bg-light d-flex">
+    <div class="row">
+        <div class="col-3 border ">
+            <img src="img/logoResto.png" class="img-fluid mx-3 mt-3 h-50 mb-3 " alt="...">
+            <button type="button" class="btn btn-secondary ">Secondary</button>
+            <button type="button" class="btn btn-secondary ">Secondary</button>
+            <button type="button" class="btn btn-secondary ">Secondary</button>
+        </div>
+        <div class="col-9">
+        Columnasdasdada
+        </div>  
+    </div>
+    </div>
+
+   
+    <!-- footer -->
+    <div class="container-fluid py-2 bg-secondary text-light">
+      <div class="container d-flex justify-content-center mb-3">
+        <label>Copyright &copy; 2022 - Manajemen Proyek A11.4503</label>
+      </div>
+    </div>
+    <!-- Akhir footer -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+  </body>
+</html>
