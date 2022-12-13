78588ed04941c3331fe054952760c813d35ba64e
Kozzen <timotiuswinzen@gmail.com>
2022-12-13 16:19:06 +0700

    fix footer

--- EditMenu.php
+++ EditMenu.php
@@ -71,11 +71,9 @@
 
    
     <!-- footer -->
-    <div class="container-fluid py-2 bg-secondary text-light">
-      <div class="container d-flex justify-content-center mb-3">
-        <label>Copyright &copy; 2022 - Manajemen Proyek A11.4503</label>
-      </div>
-    </div>
+    <footer class="bg-secondary text-white text-center mt-5 pb-2 justify-content-center">
+      <p class="pt-4">Copyright @ 2022 - Manajemen Proyek A11.4503</p>
+    </footer>
     <!-- Akhir footer -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
   </body>
